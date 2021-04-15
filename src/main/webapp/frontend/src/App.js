90667deb5aada69ed894658c6bc0d38e53b445bb
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-15 22:08:39 +0200

    Implemented base react router routing

--- src/main/webapp/frontend/src/App.js
+++ src/main/webapp/frontend/src/App.js
@@ -1,7 +1,28 @@
+import {BrowserReact as Router, Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Navbar'
+import Login from './components/Login'
+import Register from './components/Register'
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container-fluid">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
