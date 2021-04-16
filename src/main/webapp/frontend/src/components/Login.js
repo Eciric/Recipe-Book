f462dba2d8ade67be63578dfa6cb089c24765d45
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-17 00:37:24 +0200

    Added a Form to Login Component

--- src/main/webapp/frontend/src/components/Login.js
+++ src/main/webapp/frontend/src/components/Login.js
@@ -2,7 +2,25 @@
 const Login = () => {
     return (
         <div className="container">
-            <h1 className="display-2 my-5 text-center">Login component</h1>
+            <div className="card my-5 text-center">
+                <div className="card-header">
+                    Your data is safe with Recipe Book, or maybe not.
+                </div>
+                <div className="card-body">
+                    <form>
+                    <div className="form-group">
+                        <label className="my-1">Email address</label>
+                        <input type="email" className="form-control" id="inputEmail" placeholder="email@email.com..."/>
+                    </div>
+                    <div className="form-group">
+                        <label className="my-1">Password</label>
+                        <input type="password" className="form-control" id="inputPassword" placeholder="A secure password..."/>
+                    </div>
+                    <br></br>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
