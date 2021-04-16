120de6ffb2f4aa68eaa04e2f5f5c98be44a35a54
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-16 20:50:53 +0200

    Updated Login Component

---
+++ src/main/webapp/frontend/src/components/Login.js
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div className="container">
+            <h1 className="display-2 my-5 text-center">Login component</h1>
+        </div>
+    );
+}
+
+export default Login;
