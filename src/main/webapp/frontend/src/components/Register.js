ea972b6e2e91b38a2721c34d4d6c7c9a0c309dcf
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-17 00:37:40 +0200

    Added a form to Register component

--- src/main/webapp/frontend/src/components/Register.js
+++ src/main/webapp/frontend/src/components/Register.js
@@ -2,7 +2,31 @@
 const Register = () => {
     return (
         <div className="container">
-            <h1 className="display-2 my-5 text-center">Register component</h1>
+            <div className="card my-5 text-center">
+                <div className="card-header">
+                    Register your account today!
+                </div>
+                <div className="card-body">
+                    <form>
+                    <div className="form-group">
+                        <label className="my-1">Email address</label>
+                        <input type="email" className="form-control" id="inputEmail" placeholder="email@email.com..."/>
+                    </div>
+     
+                    <div className="form-group">
+                        <label className="my-1">Username</label>
+                        <input type="text" className="form-control" id="inputUsername" placeholder="A nice username..."/>
+                    </div>
+     
+                    <div className="form-group">
+                        <label className="my-1">Password</label>
+                        <input type="password" className="form-control" id="inputPassword" placeholder="A secure password..."/>
+                    </div>
+                    <br></br>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
