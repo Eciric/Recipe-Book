e95b38f870f89d0894425c43df18436411e8edd1
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-16 20:51:26 +0200

    Updated Register Component

---
+++ src/main/webapp/frontend/src/components/Register.js
@@ -0,0 +1,10 @@
+
+const Register = () => {
+    return (
+        <div className="container">
+            <h1 className="display-2 my-5 text-center">Register component</h1>
+        </div>
+    );
+}
+
+export default Register;
