6342f2d5846490ba8a8fe6b57bd5edf589bd7c38
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-16 20:50:43 +0200

    Updated Home Component

---
+++ src/main/webapp/frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="home">
+                <h1 class="display-2 text-center my-5">Home</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
