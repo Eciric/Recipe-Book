fe445ee2b169f5aca7aef54e161bb26dcd8e0cb9
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-17 00:36:54 +0200

    Fixed Imports

--- src/main/webapp/frontend/src/App.js
+++ src/main/webapp/frontend/src/App.js
@@ -1,28 +1,29 @@
-import {BrowserReact as Router, Route, Switch} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Navbar'
-import Login from './components/Login'
-import Register from './components/Register'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="container-fluid">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/register">
-            <Register />
-          </Route>
-        </Switch>
+      <div className="App">
+        <Navbar/>
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+            </Switch>
+          </div>
       </div>
     </Router>
-    
   );
 }
 
