eecc409fa42d30a56271afe4e83c1e6215bd7597
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-17 17:30:54 +0200

    Added Repository search functions

--- src/main/java/res/recipebook/Repositories/UserRepository.java
+++ src/main/java/res/recipebook/Repositories/UserRepository.java
@@ -3,5 +3,11 @@ package res.recipebook.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import res.recipebook.Models.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
