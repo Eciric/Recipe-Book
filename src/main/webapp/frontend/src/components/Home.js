b8112c490ca7d65103d7f0c44e8e249cffb53974
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-17 11:03:53 +0200

    Added Lorem ipsum to Home for testing

--- src/main/webapp/frontend/src/components/Home.js
+++ src/main/webapp/frontend/src/components/Home.js
@@ -2,7 +2,52 @@ const Home = () => {
     return (
         <div className="container">
             <div className="home">
-                <h1 class="display-2 text-center my-5">Home</h1>
+                <h1 className="display-2 text-center my-5">Home</h1>
+                <p className="text-center">
+
+Labore quia asperiores nostrum sed. Rem quaerat aut omnis fugit maxime ea. Autem qui deserunt repellat illum nihil adipisci. Quas reprehenderit omnis ullam vero distinctio ea est. Reiciendis est nostrum consectetur quae nam dolores. Cupiditate pariatur autem molestias.
+
+Eos culpa amet enim consectetur. Alias ex unde sequi. Fugit est qui illum et qui consequatur. Aut explicabo minima accusantium consequuntur. Facere at consequatur quibusdam molestias aut quod ipsam.
+
+Placeat cupiditate laborum voluptas. Consectetur eum earum et ut quia consequatur iusto. Error nulla esse sequi quas assumenda explicabo non pariatur. Cumque et quisquam rerum qui. Ad fugiat aliquam impedit excepturi officiis id. Sed sunt voluptas rerum quas eum nesciunt et odio.
+
+Autem rerum necessitatibus quia possimus. Molestiae esse qui rem consequatur culpa. Et beatae sit est numquam facere possimus quam. Sunt velit earum rem et autem dolore. Ea dolor quisquam voluptatum doloribus voluptates ducimus.
+
+Magnam qui aut perspiciatis est aut. Soluta provident dolorem beatae est ut. Minus quibusdam pariatur quidem modi laudantium eos et velit. Odit officiis distinctio cupiditate.
+
+
+Labore quia asperiores nostrum sed. Rem quaerat aut omnis fugit maxime ea. Autem qui deserunt repellat illum nihil adipisci. Quas reprehenderit omnis ullam vero distinctio ea est. Reiciendis est nostrum consectetur quae nam dolores. Cupiditate pariatur autem molestias.
+
+Eos culpa amet enim consectetur. Alias ex unde sequi. Fugit est qui illum et qui consequatur. Aut explicabo minima accusantium consequuntur. Facere at consequatur quibusdam molestias aut quod ipsam.
+
+Placeat cupiditate laborum voluptas. Consectetur eum earum et ut quia consequatur iusto. Error nulla esse sequi quas assumenda explicabo non pariatur. Cumque et quisquam rerum qui. Ad fugiat aliquam impedit excepturi officiis id. Sed sunt voluptas rerum quas eum nesciunt et odio.
+
+Autem rerum necessitatibus quia possimus. Molestiae esse qui rem consequatur culpa. Et beatae sit est numquam facere possimus quam. Sunt velit earum rem et autem dolore. Ea dolor quisquam voluptatum doloribus voluptates ducimus.
+
+Magnam qui aut perspiciatis est aut. Soluta provident dolorem beatae est ut. Minus quibusdam pariatur quidem modi laudantium eos et velit. Odit officiis distinctio cupiditate.
+
+
+Labore quia asperiores nostrum sed. Rem quaerat aut omnis fugit maxime ea. Autem qui deserunt repellat illum nihil adipisci. Quas reprehenderit omnis ullam vero distinctio ea est. Reiciendis est nostrum consectetur quae nam dolores. Cupiditate pariatur autem molestias.
+
+Eos culpa amet enim consectetur. Alias ex unde sequi. Fugit est qui illum et qui consequatur. Aut explicabo minima accusantium consequuntur. Facere at consequatur quibusdam molestias aut quod ipsam.
+
+Placeat cupiditate laborum voluptas. Consectetur eum earum et ut quia consequatur iusto. Error nulla esse sequi quas assumenda explicabo non pariatur. Cumque et quisquam rerum qui. Ad fugiat aliquam impedit excepturi officiis id. Sed sunt voluptas rerum quas eum nesciunt et odio.
+
+Autem rerum necessitatibus quia possimus. Molestiae esse qui rem consequatur culpa. Et beatae sit est numquam facere possimus quam. Sunt velit earum rem et autem dolore. Ea dolor quisquam voluptatum doloribus voluptates ducimus.
+
+Magnam qui aut perspiciatis est aut. Soluta provident dolorem beatae est ut. Minus quibusdam pariatur quidem modi laudantium eos et velit. Odit officiis distinctio cupiditate.
+
+
+Labore quia asperiores nostrum sed. Rem quaerat aut omnis fugit maxime ea. Autem qui deserunt repellat illum nihil adipisci. Quas reprehenderit omnis ullam vero distinctio ea est. Reiciendis est nostrum consectetur quae nam dolores. Cupiditate pariatur autem molestias.
+
+Eos culpa amet enim consectetur. Alias ex unde sequi. Fugit est qui illum et qui consequatur. Aut explicabo minima accusantium consequuntur. Facere at consequatur quibusdam molestias aut quod ipsam.
+
+Placeat cupiditate laborum voluptas. Consectetur eum earum et ut quia consequatur iusto. Error nulla esse sequi quas assumenda explicabo non pariatur. Cumque et quisquam rerum qui. Ad fugiat aliquam impedit excepturi officiis id. Sed sunt voluptas rerum quas eum nesciunt et odio.
+
+Autem rerum necessitatibus quia possimus. Molestiae esse qui rem consequatur culpa. Et beatae sit est numquam facere possimus quam. Sunt velit earum rem et autem dolore. Ea dolor quisquam voluptatum doloribus voluptates ducimus.
+
+Magnam qui aut perspiciatis est aut. Soluta provident dolorem beatae est ut. Minus quibusdam pariatur quidem modi laudantium eos et velit. Odit officiis distinctio cupiditate.
+</p>
             </div>
         </div>
     );
