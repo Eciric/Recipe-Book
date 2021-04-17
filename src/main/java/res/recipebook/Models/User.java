a4d412d25de96914f12b5ab4fd1cc4d2e144849d
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-17 17:29:59 +0200

    Added role column to User Model

--- src/main/java/res/recipebook/Models/User.java
+++ src/main/java/res/recipebook/Models/User.java
@@ -19,11 +19,13 @@ public class User {
     private String email;
     private String password;
     private Timestamp date_created;
+    private String role;
 
-    public User(String username, String email, String password, Timestamp date_created) {
+    public User(String username, String email, String password, Timestamp date_created, String role) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.date_created = date_created;
+        this.role = role;
     }
 }
