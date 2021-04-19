f15ce780faa4243b937390a808b711595ec582ac
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-04-19 15:25:24 +0200

    Top level component gets user data

--- src/main/webapp/frontend/src/App.js
+++ src/main/webapp/frontend/src/App.js
@@ -1,14 +1,25 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
+import {getCurrentUser} from './services/authService';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = getCurrentUser();
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
+
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        <Navbar user={currentUser}/>
           <div className="container">
             <Switch>
               <Route exact path="/">
