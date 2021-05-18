edde144839c25476f8bc3b17e017eaf7bbaa99cb
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-05-18 15:57:59 +0200

    Implemented custom Repository Query

--- src/main/java/res/recipebook/Repositories/RecipeImageRepository.java
+++ src/main/java/res/recipebook/Repositories/RecipeImageRepository.java
@@ -1,7 +1,10 @@
 package res.recipebook.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import res.recipebook.Models.RecipeImage;
 
 public interface RecipeImageRepository extends JpaRepository<RecipeImage, Integer> {
+    @Query("select r from RecipeImage r where r.recipe_id=?1")
+    RecipeImage findByRecipe_id(int recipe_id);
 }
