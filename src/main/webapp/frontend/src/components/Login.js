ea550b7977b7444573ffdbbb073d3fc64ac9a3e7
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-05-18 15:59:28 +0200

    Login component fixes

--- src/main/webapp/frontend/src/components/Login.js
+++ src/main/webapp/frontend/src/components/Login.js
@@ -1,134 +1,167 @@
-import {login} from '../services/authService';
-import {useState} from 'react';
-import {useHistory} from 'react-router-dom';
+import { login } from "../services/authService";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import image from "../images/login-side.jpg";
 
 const Login = () => {
     const history = useHistory();
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [message, setMessage] = useState('');
-    const [usernameError, setUsernameError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-    const [successful, setSuccessful] = useState('');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [usernameError, setUsernameError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [successful, setSuccessful] = useState("");
     const [loading, setLoading] = useState(false);
 
     const handleUsernameChange = (e) => {
-        setUsername(e.target.value)
+        setUsername(e.target.value);
         if (e.target.value) {
-            setUsernameError('');
-            e.target.style.border='1px solid #683ed1';
+            setUsernameError("");
+            e.target.style.border = "1px solid #683ed1";
         }
 
         if (!e.target.value) {
-            setUsernameError('Username can\'t be empty!');
-            e.target.style.border='2px solid red';
+            setUsernameError("Username can't be empty!");
+            e.target.style.border = "2px solid red";
         }
     };
 
     const handlePasswordChange = (e) => {
-        setPassword(e.target.value)
+        setPassword(e.target.value);
         if (e.target.value) {
-            setPasswordError('');
-            e.target.style.border='1px solid #683ed1';
+            setPasswordError("");
+            e.target.style.border = "1px solid #683ed1";
         }
 
         if (!e.target.value) {
-            setPasswordError('Password can\'t be empty!');
-            e.target.style.border='2px solid red';
+            setPasswordError("Password can't be empty!");
+            e.target.style.border = "2px solid red";
         }
-
     };
 
     const handleLogin = (e) => {
         e.preventDefault();
-        
-        if (!username) setUsernameError('Username field can\'t be empty!');
-        if (!password) setPasswordError('Password field can\'t be empty!');
-        
-        if (!usernameError.length && !passwordError.length && username.length && password.length) {
+
+        if (!username) setUsernameError("Username field can't be empty!");
+        if (!password) setPasswordError("Password field can't be empty!");
+
+        if (
+            !usernameError.length &&
+            !passwordError.length &&
+            username.length &&
+            password.length
+        ) {
             setLoading(true);
-            login(username, password)
-            .then(
+            login(username, password).then(
                 () => {
                     setLoading(false);
                     setSuccessful(true);
-                    setMessage('Login successful!');
+                    setMessage("Login successful!");
                     setTimeout(() => {
-                        history.push("/")
+                        history.push("/");
                         window.location.reload();
                     }, 500);
                 },
-                error => {
-                    const resMessage = 
-                        (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
-            
+                (error) => {
+                    const resMessage =
+                        (error.response &&
+                            error.response.data &&
+                            error.response.data.message) ||
+                        error.message ||
+                        error.toString();
+
                     setLoading(false);
                     setSuccessful(false);
                     setMessage(resMessage);
                 }
-            )
+            );
         }
-    }
+    };
 
     return (
         <div className="container">
-            <div className="card my-5 text-center">
+            <div className="card text-center">
                 <div className="card-header">
                     Your data is safe with Recipe Book, or maybe not.
                 </div>
-                <div className="card-body">
-                    <form onSubmit={handleLogin}>
-                    <div className="form-group">
-                        <label className="my-1">Username</label>
-                        <input 
-                            type="text" 
-                            className="form-control auth-control" 
-                            id="inputUsername" 
-                            placeholder="Your cool username..."
-                            onChange={handleUsernameChange}
-                            value={username}/>
+                <div className="card-body ">
+                    <div className="row align-items-center">
+                        <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <img
+                                className="img-fluid login-image"
+                                src={image}
+                                alt=""
+                            ></img>
+                        </div>
+                        <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <form onSubmit={handleLogin}>
+                                <div className="form-group my-5">
+                                    <label className="my-2">Username</label>
+                                    <input
+                                        type="text"
+                                        className="form-control auth-control"
+                                        id="inputUsername"
+                                        placeholder="Your cool username..."
+                                        onChange={handleUsernameChange}
+                                        value={username}
+                                    />
 
-                        {usernameError.length > 0 &&
-                            <span className='error'>{usernameError}</span>
-                        }
+                                    {usernameError.length > 0 && (
+                                        <span className="error">
+                                            {usernameError}
+                                        </span>
+                                    )}
+                                </div>
+
+                                <div className="form-group my-5">
+                                    <label className="my-2">Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control auth-control"
+                                        id="inputPassword"
+                                        placeholder="Your secure password..."
+                                        onChange={handlePasswordChange}
+                                        value={password}
+                                    />
+
+                                    {passwordError.length > 0 && (
+                                        <span className="error">
+                                            {passwordError}
+                                        </span>
+                                    )}
+                                </div>
+                                <br></br>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary my-3"
+                                    disabled={loading}
+                                >
+                                    Login!
+                                </button>
+                            </form>
+
+                            {message.length > 0 &&
+                                (successful ? (
+                                    <div
+                                        className="alert alert-success my-3"
+                                        role="alert"
+                                    >
+                                        <p>{message}</p>
+                                    </div>
+                                ) : (
+                                    <div
+                                        className="alert alert-danger my-3"
+                                        role="alert"
+                                    >
+                                        <p>{message}</p>
+                                    </div>
+                                ))}
+                        </div>
                     </div>
-
-                    <div className="form-group my-2">
-                        <label className="my-1">Password</label>
-                        <input 
-                            type="password" 
-                            className="form-control auth-control"
-                            id="inputPassword" 
-                            placeholder="Your secure password..."
-                            onChange={handlePasswordChange}
-                            value={password}/>
-                        
-                        {passwordError.length > 0 &&
-                            <span className='error'>{passwordError}</span>
-                        }
-                    </div>
-                    <br></br>
-                    <button type="submit" className="btn btn-primary my-3" disabled={loading}>Login!</button>
-                    </form>
-
-                    {message.length > 0 && ( successful
-                        ?
-                            (
-                            <div className="alert alert-success my-3" role="alert">
-                                <p>{message}</p>
-                            </div>
-                            )
-                        :   
-                            (
-                            <div className="alert alert-danger my-3" role="alert">
-                                <p>{message}</p>
-                            </div>
-                            )
-                    )}
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default Login;
