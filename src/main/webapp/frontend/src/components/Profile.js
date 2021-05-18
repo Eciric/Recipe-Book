5c3a17aa50efedaf93616b7d59cb75e14ab65421
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-05-18 16:00:02 +0200

    Cleaned up the profile component

--- src/main/webapp/frontend/src/components/Profile.js
+++ src/main/webapp/frontend/src/components/Profile.js
@@ -1,57 +1,62 @@
-import defaultImage from '../images/user.png';
-import {useEffect, useRef, useState} from 'react';
-import {useParams} from 'react-router-dom';
-import logo from '../images/book.png';
-import {downloadProfilePicture, uploadProfilePicture} from '../services/fileService';
-import Loader from 'react-loader-spinner';
-
-// Black magic and wizardy, but its the only way for this to work.
-function base64toBlob(base64Data, contentType) {
-    var sliceSize = 1024;
-    var byteCharacters = atob(base64Data);
-    var bytesLength = byteCharacters.length;
-    var slicesCount = Math.ceil(bytesLength / sliceSize);
-    var byteArrays = new Array(slicesCount);
-    for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-        var begin = sliceIndex * sliceSize;
-        var end = Math.min(begin + sliceSize, bytesLength);
-        var bytes = new Array(end - begin);
-        for (var offset = begin, i = 0 ; offset < end; ++i, ++offset) {
-            bytes[i] = byteCharacters[offset].charCodeAt(0);
-        }
-        byteArrays[sliceIndex] = new Uint8Array(bytes);
-    }
-    return new Blob(byteArrays, { type: contentType });
-}
+import defaultImage from "../images/user.png";
+import { base64toBlob } from "../services/base64ToBlob";
+import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+    downloadProfilePicture,
+    uploadProfilePicture,
+} from "../services/fileService";
+import Loader from "react-loader-spinner";
+import RecipeTile from "./RecipeTile";
+import pancakes from "../images/pancakes.jpg";
+import brownies from "../images/brownies.jpg";
+import cookies from "../images/cookies.jpg";
 
 const Profile = () => {
-    const [errorMessage, setErrorMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState("");
     const [loading, setLoading] = useState(false);
     const [profileUri, setProfileUri] = useState(defaultImage);
     const [userRecipes, setUserRecipes] = useState([]);
-    const {username} = useParams();
+    const { username } = useParams();
     const inputFile = useRef(null);
     const onButtonClick = () => {
-       inputFile.current.click();
+        inputFile.current.click();
     };
 
     useEffect(() => {
         setLoading(true);
+        setUserRecipes([
+            {
+                id: 1,
+                title: "Pancakes",
+                img: pancakes,
+            },
+            {
+                id: 2,
+                title: "Brownies",
+                img: brownies,
+            },
+            {
+                id: 3,
+                title: "Chocolate Cookies",
+                img: cookies,
+            },
+        ]);
         downloadProfilePicture()
-        .then(res => {
-            if (res.status === 200) {
-                let blob = base64toBlob(res.data.bytes, "image/png");
-                const objectURL = URL.createObjectURL(blob)
-                setProfileUri(objectURL);
-            } else {
-                setProfileUri(defaultImage);
-            }
-            setLoading(false);
-        })
-        .catch(err => {
-            setErrorMessage(err);
-            setLoading(false);
-        })
+            .then((res) => {
+                if (res.status === 200) {
+                    let blob = base64toBlob(res.data.bytes, "image/png");
+                    const objectURL = URL.createObjectURL(blob);
+                    setProfileUri(objectURL);
+                } else {
+                    setProfileUri(defaultImage);
+                }
+                setLoading(false);
+            })
+            .catch((err) => {
+                setErrorMessage(err);
+                setLoading(false);
+            });
     }, []);
 
     const onFileChange = (event) => {
@@ -61,110 +66,123 @@ const Profile = () => {
         if (file) {
             setLoading(true);
             uploadProfilePicture(file)
-            .then(res => {
-                if (res.ok) {
-                    const reader = new FileReader();
-                    reader.addEventListener("load", function () {
-                        setProfileUri(reader.result);
-                        setLoading(false);
-                    }, false);
-                
-                    if (file) {
-                        reader.readAsDataURL(file);
+                .then((res) => {
+                    if (res.ok) {
+                        const reader = new FileReader();
+                        reader.addEventListener(
+                            "load",
+                            function () {
+                                setProfileUri(reader.result);
+                                setLoading(false);
+                            },
+                            false
+                        );
+
+                        if (file) {
+                            reader.readAsDataURL(file);
+                        }
                     }
-                }
-            })
-            .catch(err => {
-                setErrorMessage(err);
-                setLoading(false);
-            })
+                })
+                .catch((err) => {
+                    setErrorMessage(err);
+                    setLoading(false);
+                });
         }
     };
 
     return (
         <div id="profileContainer" className="container my-5">
             <div className="profilePictureContainer ">
-                {loading ? 
-                    <Loader type="ThreeDots" color="#683ED1" height="100" width="100" />
-                    :
-                    (<div>
-                        <img id="profilePicture" src={profileUri} alt="" className="img-fluid my-4"/>
+                {loading ? (
+                    <Loader
+                        type="ThreeDots"
+                        color="#683ED1"
+                        height="100"
+                        width="100"
+                    />
+                ) : (
+                    <div>
+                        <img
+                            id="profilePicture"
+                            src={profileUri}
+                            alt=""
+                            className="img-fluid my-4"
+                        />
                         <div className="addProfilePictureContainer">
                             <span className="fa-stack">
-                                <i id="backgroundIcon" className="fa fa-circle fa-stack-1x"></i>
-                                <i id="addProfilePictureIcon" className="fa fa-plus-circle fa-stack-1x" onClick={onButtonClick}>
-                                    <input type='file' id='file' ref={inputFile} onChange={onFileChange} style={{display: 'none'}}/>
+                                <i
+                                    id="backgroundIcon"
+                                    className="fa fa-circle fa-stack-1x"
+                                ></i>
+                                <i
+                                    id="addProfilePictureIcon"
+                                    className="fa fa-plus-circle fa-stack-1x"
+                                    onClick={onButtonClick}
+                                >
+                                    <input
+                                        type="file"
+                                        id="file"
+                                        ref={inputFile}
+                                        onChange={onFileChange}
+                                        style={{ display: "none" }}
+                                    />
                                 </i>
                             </span>
-                        </div>          
-                    </div>)
-               }
-                
+                        </div>
+                        {errorMessage.length > 0 && (
+                            <div
+                                className="alert alert-danger my-3"
+                                role="alert"
+                            >
+                                <p>{errorMessage}</p>
+                            </div>
+                        )}
+                    </div>
+                )}
             </div>
-            <h1 className="display-4" style={{color:'#683ed1'}}>{username}</h1>
+            <h1 className="display-4" style={{ color: "#683ed1" }}>
+                {username}
+            </h1>
             <p>
                 Recipes shared: {0}
                 <br></br>
                 All recipe likes: {0}
             </p>
-            
 
             <hr className="my-5"></hr>
             <div className="container ">
-                <h1 className="display-4 text-center my-5" style={{color:'#683ed1'}}>My recipes</h1>
-                {userRecipes.length ? 
-                    <div className="row justify-content-center mx-5 text-center padding">
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Strawberry Pancakes</h4>
+                <h1
+                    className="display-4 text-center my-5"
+                    style={{ color: "#683ed1" }}
+                >
+                    My recipes
+                </h1>
+                {userRecipes.length ? (
+                    <div className="row justify-content-around">
+                        {userRecipes.map((recipe) => {
+                            let title = recipe.title;
+                            let img = recipe.img;
+                            return <RecipeTile title={title} img={img} />;
+                        })}
                     </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Brownies</h4>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Poppyseed Cake</h4>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Strawberry Pancakes</h4>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Brownies</h4>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Poppyseed Cake</h4>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Strawberry Pancakes</h4>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Brownies</h4>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 my-2">
-                        <img className='img-fluid' src={logo} alt=""/>
-                        <h4>Poppyseed Cake</h4>
-                    </div>
-                    </div>
-                    :
-                    (<div className="mb-5">
+                ) : (
+                    <div className="mb-5">
                         <p>No recipes? Add some now!</p>
-                        <a href='/recipecreator' className='my-1'>
-                            <button type="button" className="btn btn-primary my-3">Create your first recipe!</button>
+                        <a href="/recipecreator" className="my-1">
+                            <button
+                                type="button"
+                                className="btn btn-primary my-3"
+                            >
+                                Create your first recipe!
+                            </button>
                         </a>
-                    </div>)
-                }
-                
+                    </div>
+                )}
             </div>
 
             <br></br>
         </div>
     );
-}
+};
 
 export default Profile;
