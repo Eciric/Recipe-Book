a44eb88c70a7edd014316d611d463f3bfde6e222
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2021-05-18 15:57:37 +0200

    Implemented Recipe Controller for handling recipe storing and fetching

--- src/main/java/res/recipebook/Controllers/RecipeController.java
+++ src/main/java/res/recipebook/Controllers/RecipeController.java
@@ -1,18 +1,49 @@
 package res.recipebook.Controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import res.recipebook.Services.FileStorageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import res.recipebook.Payload.Responses.RecipeResponse;
+import res.recipebook.Repositories.UserRepository;
+import res.recipebook.Security.Services.UserDetailsImpl;
 import res.recipebook.Services.RecipeService;
 
 @CrossOrigin(origins="*")
 @RestController
 @RequestMapping(path="/api/recipes")
 public class RecipeController {
-    private final RecipeService recipeStorageService;
+    private final RecipeService recipeService;
+    private final UserRepository userRepository;
 
-    public RecipeController(RecipeService recipeStorageService) {
-        this.recipeStorageService = recipeStorageService;
+    public RecipeController(RecipeService recipeService, UserRepository userRepository) {
+        this.recipeService = recipeService;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping(value = "/storeRecipe", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> storeRecipe(@RequestParam(value = "file") MultipartFile file,
+                                         @RequestParam(value="title") String title,
+                                         @RequestParam(value="contents") String contents) {
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        try {
+            UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (userRepository.existsByUsername(userDetails.getUsername()) && userRepository.existsById(userDetails.getId().intValue())) {
+                boolean ok = recipeService.storeRecipe(userDetails.getId().intValue(), file, title, contents);
+                if (ok) return new ResponseEntity<>(HttpStatus.OK);
+                else return new ResponseEntity<>(HttpStatus.FAILED_DEPENDENCY);
+            } else {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(value = "/getAllRecipes", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public RecipeResponse getAllRecipes() {
+        return recipeService.getAllRecipes();
     }
 }
