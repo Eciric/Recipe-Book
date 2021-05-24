c1475f3183b8f75cf1129427e8c0b56cbdd38e9b
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-05-24 19:55:40 +0200

    RecipeRepository now allows finding recipes by user id

--- src/main/java/res/recipebook/Repositories/RecipeRepository.java
+++ src/main/java/res/recipebook/Repositories/RecipeRepository.java
@@ -1,7 +1,12 @@
 package res.recipebook.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import res.recipebook.Models.Recipe;
 
+import java.util.List;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+    @Query("select r from Recipe r where r.user_id=?1")
+    List<Recipe> findAllByUser_id(int user_id);
 }
