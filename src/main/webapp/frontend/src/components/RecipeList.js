08cb18ec086972603640d556f5f35bdcd95015ca
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-05-24 19:58:55 +0200

    Created RecipeList component for refactoring

---
+++ src/main/webapp/frontend/src/components/RecipeList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import RecipeTile from "./RecipeTile";
+import Loader from "react-loader-spinner";
+
+export const RecipeList = ({ loading, recipes, noRecipesComponent }) => {
+    return (
+        <section className="recipeList">
+            {loading ? (
+                <div className="spinnerContainer">
+                    <Loader
+                        type="MutatingDots"
+                        color="#683ED1"
+                        secondaryColor="#9b6dff"
+                        height={100}
+                        width={100}
+                    />
+                </div>
+            ) : recipes.length ? (
+                <div className="row justify-content-around">
+                    {recipes.map((recipe) => {
+                        return (
+                            <RecipeTile
+                                title={recipe.title}
+                                img={recipe.img}
+                                key={recipe.id}
+                            />
+                        );
+                    })}
+                </div>
+            ) : (
+                noRecipesComponent
+            )}
+        </section>
+    );
+};
