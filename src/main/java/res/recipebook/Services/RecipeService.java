47520f8caed8c868869e4f1fb52684fbdd34cc34
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-05-24 19:56:51 +0200

    Refactoring the Recipe Service and Implementing returning more recipes

--- src/main/java/res/recipebook/Services/RecipeService.java
+++ src/main/java/res/recipebook/Services/RecipeService.java
@@ -21,6 +21,7 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class RecipeService {
@@ -42,12 +43,24 @@ public class RecipeService {
 
     // Get a recipe by id
     public RecipeResponse getRecipeById(int id) {
-        return null;
+        Optional<Recipe> recipe = recipeRepository.findById(id);
+        if (recipe.isPresent()) {
+            RecipeResponse recipeResponse = new RecipeResponse();
+            Recipes[] recipes = new Recipes[1];
+            recipes[0] = initializeRecipesObject(recipe.get());
+            recipeResponse.setRecipes(recipes);
+            return recipeResponse;
+        } else {
+            return null;
+        }
     }
 
     // Get all recipes of requested user
     public RecipeResponse getRecipesOfUser(int userID) {
-        return null;
+        List<Recipe> userRecipes = recipeRepository.findAllByUser_id(userID);
+        if (userRecipes.size()==0) return new RecipeResponse();
+
+        return initializeRecipeResponseObject(userRecipes);
     }
 
     // Get all recipes
@@ -55,30 +68,9 @@ public class RecipeService {
         List<Recipe> recipesList = recipeRepository.findAll();
         if (recipesList.size()==0) return new RecipeResponse();
 
-        Recipes[] allRecipesWithImages = new Recipes[recipesList.size()];
-        for (int i = 0; i < recipesList.size(); i++) {
-            Recipes newRecipes = new Recipes();
-            newRecipes.setRecipeData(recipesList.get(i));
-            int recipe_id = recipesList.get(i).getRecipe_id();
-            RecipeImage recipeImage = recipeImageRepository.findByRecipe_id(recipe_id);
-            String filePath = recipeImage.getImage_path();
-            File file = new File(filePath);
-            byte[] bytes = new byte[0];
-            try {
-                bytes = Files.readAllBytes(Path.of(filePath));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            newRecipes.setFile(file);
-            newRecipes.setImage(bytes);
-            allRecipesWithImages[i] = newRecipes;
-        }
-        RecipeResponse recipeResponse = new RecipeResponse();
-        recipeResponse.setRecipes(allRecipesWithImages);
-        return recipeResponse;
+        return initializeRecipeResponseObject(recipesList);
     }
 
-    // TODO: Saving implementation
     // Save a new recipe
     public boolean storeRecipe(int user_id, MultipartFile file, String title, String contents) {
         String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
@@ -96,5 +88,35 @@ public class RecipeService {
             System.out.println(e.getMessage());
             return false;
         }
+    }
+
+    private Recipes initializeRecipesObject(Recipe recipe) {
+        Recipes recipes = new Recipes();
+        recipes.setRecipeData(recipe);
+        int recipe_id = recipe.getRecipe_id();
+        RecipeImage recipeImage = recipeImageRepository.findByRecipe_id(recipe_id);
+        String filePath = recipeImage.getImage_path();
+        File file = new File(filePath);
+        byte[] bytes = new byte[0];
+        try {
+            bytes = Files.readAllBytes(Path.of(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recipes.setFile(file);
+        recipes.setImage(bytes);
+        return recipes;
+    }
+
+    private RecipeResponse initializeRecipeResponseObject(List<Recipe> recipeList) {
+        Recipes[] allUserRecipesWithImages = new Recipes[recipeList.size()];
+        for (int i = 0; i < recipeList.size(); i++) {
+            Recipes newRecipes = initializeRecipesObject(recipeList.get(i));
+            newRecipes.getRecipeData().setRecipe_text("");
+            allUserRecipesWithImages[i] = newRecipes;
+        }
+        RecipeResponse recipeResponse = new RecipeResponse();
+        recipeResponse.setRecipes(allUserRecipesWithImages);
+        return recipeResponse;
     }
 }
