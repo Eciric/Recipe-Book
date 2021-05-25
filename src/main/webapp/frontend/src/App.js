7e6aca62fa4b059e08467cc57b38572d2028d60a
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-05-25 15:27:22 +0200

    App component indenting change

--- src/main/webapp/frontend/src/App.js
+++ src/main/webapp/frontend/src/App.js
@@ -1,49 +1,53 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {useState, useEffect} from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import Profile from './components/Profile';
-import Footer from './components/Footer';
-import RecipeCreator from './components/RecipeCreator';
-import {getCurrentUser} from './services/authService';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
+import { RecipeView } from "./components/RecipeView";
+import RecipeCreator from "./components/RecipeCreator";
+import { getCurrentUser } from "./services/authService";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(undefined);
+    const [currentUser, setCurrentUser] = useState(undefined);
 
-  useEffect(() => {
-    const user = getCurrentUser();
-    if (user) {
-      setCurrentUser(user);
-    }
-  }, []);
+    useEffect(() => {
+        const user = getCurrentUser();
+        if (user) {
+            setCurrentUser(user);
+        }
+    }, []);
 
-  return (
-    <Router>
-      <Navbar user={currentUser}/>
-        <div className="container-fluid">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/profile/:username">
-              <Profile />
-            </Route>
-            <Route path="/recipecreator">
-              <RecipeCreator />
-            </Route>
-          </Switch>
-        </div>
-        <Footer/>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar user={currentUser} />
+            <div className="container-fluid">
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/register">
+                        <Register />
+                    </Route>
+                    <Route path="/profile/:username">
+                        <Profile />
+                    </Route>
+                    <Route path="/recipecreator">
+                        <RecipeCreator />
+                    </Route>
+                    <Route path="/recipeview/:id">
+                        <RecipeView />
+                    </Route>
+                </Switch>
+            </div>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
