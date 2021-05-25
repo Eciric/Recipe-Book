8b134b6a239dc91321f5b71ca7a3a9788ad4f296
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-05-25 15:26:39 +0200

    Added RecipeRequest payload

---
+++ src/main/java/res/recipebook/Payload/Requests/RecipeRequest.java
@@ -0,0 +1,10 @@
+package res.recipebook.Payload.Requests;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RecipeRequest {
+    private int id;
+}
