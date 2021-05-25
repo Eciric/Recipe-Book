04d72f628ca2a012d5b49e0e0f29d80217306d2f
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-05-25 15:28:54 +0200

    Changed the body of certain fetch requests

--- src/main/webapp/frontend/src/services/recipeService.js
+++ src/main/webapp/frontend/src/services/recipeService.js
@@ -2,6 +2,7 @@ import authHeader from "../services/authHeader";
 
 const API_UPLOAD_URL = "http://localhost:8080/api/recipes/storeRecipe";
 const API_DOWNLOAD_ALL_URL = "http://localhost:8080/api/recipes/getAllRecipes";
+const API_GET_RECIPE_URL = "http://localhost:8080/api/recipes/getRecipeById";
 const API_GET_USER_RECIPES_URL =
     "http://localhost:8080/api/recipes/getAllUserRecipes";
 
@@ -27,12 +28,28 @@ export const getAllRecipes = async () => {
     return res;
 };
 
-export const getAllUserRecipes = async (id) => {
-    const formData = new FormData();
-    formData.append("id", id);
+export const getAllUserRecipes = async (username) => {
     const res = await fetch(API_GET_USER_RECIPES_URL, {
         method: "post",
-        body: formData,
+        body: JSON.stringify({
+            id: id,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return res;
+};
+
+export const getRecipeById = async (id) => {
+    const res = await fetch(API_GET_RECIPE_URL, {
+        method: "post",
+        body: JSON.stringify({
+            id: id,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
     });
     return res;
 };
