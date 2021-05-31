84c5e2b854aee706cc2762060404119c977d83aa
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-01 00:52:15 +0200

    Created request payload for requesting user data

---
+++ src/main/java/res/recipebook/Payload/Requests/UserRequest.java
@@ -0,0 +1,10 @@
+package res.recipebook.Payload.Requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserRequest {
+    private String username;
+    private int id;
+}
