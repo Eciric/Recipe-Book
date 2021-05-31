bdc5685943614c68fd013576ba0ebd8df441bf06
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-01 00:57:45 +0200

    Formatting update to fileService

--- src/main/webapp/frontend/src/services/fileService.js
+++ src/main/webapp/frontend/src/services/fileService.js
@@ -1,24 +1,15 @@
-import axios from 'axios';
-import authHeader from '../services/authHeader';
+import authHeader from "../services/authHeader";
 
-const API_UPLOAD_URL = 'http://localhost:8080/api/files/uploadProfilePicture';
-const API_DOWNLOAD_URL = 'http://localhost:8080/api/files/downloadProfilePicture';
+const API_UPLOAD_URL = "http://localhost:8080/api/files/uploadProfilePicture";
 
 export const uploadProfilePicture = async (file) => {
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
     const res = await fetch(API_UPLOAD_URL, {
-        method: 'post',
+        method: "post",
         body: formData,
-        headers: authHeader()
+        headers: authHeader(),
     });
 
     return res;
-}
-
-export const downloadProfilePicture = async () => {
-    const res = await axios.get(API_DOWNLOAD_URL, {
-        headers: authHeader()
-    });
-    return res;
-}
+};
