dc38f7b522ac31ad4ade7bd2c178918b998d00bf
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-01 00:58:13 +0200

    Added method to fetch specific user data to userService

--- src/main/webapp/frontend/src/services/userService.js
+++ src/main/webapp/frontend/src/services/userService.js
@@ -1,4 +1,17 @@
+const API_GET_USER_DATA_URL = "http://localhost:8080/api/users/getUserData";
+
 export const isLoggedIn = () => {
     const user = localStorage.getItem("user");
     return user !== null && user !== undefined;
+};
+
+export const getUserData = async (username, id) => {
+    const res = fetch(API_GET_USER_DATA_URL, {
+        method: "post",
+        body: JSON.stringify({ username: username, id: id }),
+        headers: { "Content-Type": "application/json" },
+    }).then((res) => {
+        return res.json();
+    });
+    return res;
 };
