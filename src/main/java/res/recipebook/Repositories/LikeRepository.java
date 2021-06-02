c3976c6e56bcc3b3836de0776d98a7c2c926b6aa
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-02 20:59:57 +0200

    Added new queries to LikeRepository

--- src/main/java/res/recipebook/Repositories/LikeRepository.java
+++ src/main/java/res/recipebook/Repositories/LikeRepository.java
@@ -1,7 +1,15 @@
 package res.recipebook.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import res.recipebook.Models.Like;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Like, Integer> {
+    @Query("select r from Like r where r.user_id=?1")
+    List<Like> findAllByUser_id(int user_id);
+
+    @Query("select r from Like r where r.recipe_id=?1")
+    List<Like> findAllByRecipe_id(int recipe_id);
 }
