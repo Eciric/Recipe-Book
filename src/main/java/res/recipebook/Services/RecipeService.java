998037971d04d90c3d998d23df149379994fcdec
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-02 21:04:29 +0200

    Deleting recipe added to service layer

--- src/main/java/res/recipebook/Services/RecipeService.java
+++ src/main/java/res/recipebook/Services/RecipeService.java
@@ -119,4 +119,8 @@ public class RecipeService {
         recipeResponse.setRecipes(allUserRecipesWithImages);
         return recipeResponse;
     }
+
+    public void deleteRecipeById(int id) {
+        recipeRepository.deleteById(id);
+    }
 }
