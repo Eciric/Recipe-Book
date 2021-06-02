a7d0d3a12789b33aa14cdc58df141b6718aac64e
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-02 21:31:00 +0200

    Added like count updating in the DB after each like request

--- src/main/java/res/recipebook/Services/LikeService.java
+++ src/main/java/res/recipebook/Services/LikeService.java
@@ -2,17 +2,22 @@ package res.recipebook.Services;
 
 import org.springframework.stereotype.Service;
 import res.recipebook.Models.Like;
+import res.recipebook.Models.Recipe;
 import res.recipebook.Repositories.LikeRepository;
+import res.recipebook.Repositories.RecipeRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LikeService {
 
     private final LikeRepository likeRepository;
+    private final RecipeRepository recipeRepository;
 
-    public LikeService(LikeRepository likeRepository) {
+    public LikeService(LikeRepository likeRepository, RecipeRepository recipeRepository) {
         this.likeRepository = likeRepository;
+        this.recipeRepository = recipeRepository;
     }
 
     public List<Like> getUserLikes(int user_id) {
@@ -28,6 +33,7 @@ public class LikeService {
         like.setUser_id(user_id);
         like.setRecipe_id(recipe_id);
         likeRepository.save(like);
+        updateRecipeLikes(recipe_id);
     }
 
     public void removeUserLike(int user_id, int recipe_id) {
@@ -36,10 +42,19 @@ public class LikeService {
         for (Like userLike : userLikes) {
             if (userLike.getRecipe_id() == recipe_id) {
                 likeRepository.deleteById(userLike.getLike_id());
+                updateRecipeLikes(recipe_id);
                 break;
             }
         }
     }
 
-
+    public void updateRecipeLikes(int recipe_id) {
+        Optional<Recipe> recipe = recipeRepository.findById(recipe_id);
+        if (recipe.isPresent()) {
+            List<Like> likes = likeRepository.findAllByRecipe_id(recipe_id);
+            int likesCount = likes.size();
+            recipe.get().setLikes(likesCount);
+            recipeRepository.save(recipe.get());
+        }
+    }
 }
