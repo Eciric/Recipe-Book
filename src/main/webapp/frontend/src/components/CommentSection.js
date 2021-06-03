5a64869f80bddf18ddb460770579dd09bf91ddd0
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-04 01:09:38 +0200

    Created CommentSection component for displaying comments under a recipe

---
+++ src/main/webapp/frontend/src/components/CommentSection.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+export const CommentSection = ({ comments }) => {
+    return (
+        <div className="container allComments">
+            {comments.map((comment) => {
+                let userImage = comment.image;
+                let username = comment.username;
+                let message = comment.message;
+                let date = new Date(comment.date).toLocaleDateString("en-gb", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                });
+                let id = comment.id;
+                return (
+                    <div key={id} className="container comment my-5">
+                        <div className="image">
+                            <img
+                                id="recipeUserPicture"
+                                alt=""
+                                src={userImage}
+                            />
+                        </div>
+                        <div className="text">
+                            <div className="commentMeta">
+                                <p id="name">{username}</p>
+                                <p id="date">{date}</p>
+                            </div>
+
+                            <div className="messageContent  text-break">
+                                {message}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
