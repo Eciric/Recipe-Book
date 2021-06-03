e5a67e51e9ab9e176d6f495d7278d9d3319be306
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-04 01:08:03 +0200

    Added comment controller for handling comment requests

---
+++ src/main/java/res/recipebook/Controllers/CommentController.java
@@ -0,0 +1,43 @@
+package res.recipebook.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import res.recipebook.Models.Comment;
+import res.recipebook.Payload.Requests.CommentRequest;
+import res.recipebook.Services.CommentService;
+
+import java.util.List;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping(value="/getAllComments", produces={MediaType.APPLICATION_JSON_VALUE})
+    public List<Comment> getAllComments(@RequestBody CommentRequest request) {
+        return commentService.getAllComments(request.getRecipe_id());
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping(value="/addComment")
+    public ResponseEntity<?> addComment(@RequestBody CommentRequest request) {
+        commentService.addComment(request.getUser_id(), request.getRecipe_id(), request.getUsername(), request.getMessage());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @DeleteMapping(value="/deleteComment")
+    public ResponseEntity<?> deleteComment(@RequestBody CommentRequest request) {
+        commentService.deleteComment(request.getComment_id());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
