86373532a670a7b8fd0814e31a4b1063e6f459d8
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-04 01:08:25 +0200

    Added new query to comment repository

--- src/main/java/res/recipebook/Repositories/CommentRepository.java
+++ src/main/java/res/recipebook/Repositories/CommentRepository.java
@@ -1,7 +1,12 @@
 package res.recipebook.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import res.recipebook.Models.Comment;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    @Query("select r from Comment r where r.recipe_id=?1")
+    List<Comment> findAllByRecipe_id(int recipe_id);
 }
