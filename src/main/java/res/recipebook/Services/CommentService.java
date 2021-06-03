1e92250043f3bd18024aaca3a126f57e497ead3b
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-04 01:08:52 +0200

    Created service layer for handling comment logic

---
+++ src/main/java/res/recipebook/Services/CommentService.java
@@ -0,0 +1,32 @@
+package res.recipebook.Services;
+
+import org.springframework.stereotype.Service;
+import res.recipebook.Models.Comment;
+import res.recipebook.Repositories.CommentRepository;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getAllComments(int recipe_id) {
+        return commentRepository.findAllByRecipe_id(recipe_id);
+    }
+
+    public void addComment(int user_id, int recipe_id, String username, String message) {
+        commentRepository.save(new Comment(recipe_id, user_id, username, message, Timestamp.from(Instant.now())));
+    }
+
+    public void deleteComment(int comment_id) {
+        Optional<Comment> comment = commentRepository.findById(comment_id);
+        comment.ifPresent(commentRepository::delete);
+    }
+}
