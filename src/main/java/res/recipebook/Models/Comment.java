530f2babb022a4456d8b6019571858cf1a916448
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-04 01:07:47 +0200

    Added username to comments and changed the size of the message to 1000 chars

--- src/main/java/res/recipebook/Models/Comment.java
+++ src/main/java/res/recipebook/Models/Comment.java
@@ -3,6 +3,7 @@ package res.recipebook.Models;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 
 @Entity
@@ -19,12 +20,15 @@ public class Comment {
 
     private int recipe_id;
     private int user_id;
+    private String username;
+    @Size(max = 1000)
     private String message;
     private Timestamp date_created;
 
-    public Comment(int recipe_id, int user_id, String message, Timestamp date_created) {
+    public Comment(int recipe_id, int user_id, String username, String message, Timestamp date_created) {
         this.recipe_id = recipe_id;
         this.user_id = user_id;
+        this.username = username;
         this.message = message;
         this.date_created = date_created;
     }
