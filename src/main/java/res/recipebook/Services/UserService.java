8247e4e3d25de904acfbc555f5017796fd39f9d0
Przemyslaw Kaczmarski <nyviel111@gmail.com>
2021-06-05 15:54:40 +0200

    Created the update method in service layer

--- src/main/java/res/recipebook/Services/UserService.java
+++ src/main/java/res/recipebook/Services/UserService.java
@@ -1,27 +1,33 @@
 package res.recipebook.Services;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import res.recipebook.Models.ERole;
+import res.recipebook.Models.Role;
 import res.recipebook.Models.User;
 import res.recipebook.Payload.Responses.UserResponse;
 import res.recipebook.Repositories.RecipeImageRepository;
+import res.recipebook.Repositories.RoleRepository;
 import res.recipebook.Repositories.UserRepository;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final RecipeImageRepository recipeImageRepository;
+    private final RoleRepository roleRepository;
 
-    public UserService(UserRepository userRepository, RecipeImageRepository recipeImageRepository) {
+    public UserService(UserRepository userRepository, RecipeImageRepository recipeImageRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.recipeImageRepository = recipeImageRepository;
+        this.roleRepository = roleRepository;
     }
 
     public UserResponse getUserDataByUsername(String username) {
@@ -59,5 +65,31 @@ public class UserService {
 
     public void deleteUserById(int id) {
         userRepository.deleteById(id);
+    }
+
+    public void updateUser(int user_id, String username, Set<String> roles, String email) {
+        Optional<User> user = userRepository.findById(user_id);
+        if (user.isPresent()) {
+            user.get().setUsername(username);
+            user.get().setRoles(initializeRoles(roles));
+            user.get().setEmail(email);
+            userRepository.save(user.get());
+        }
+    }
+
+    public Set<Role> initializeRoles(Set<String> roles) {
+        Set<Role> newRoles = new HashSet<>();
+        roles.forEach(role -> {
+            if ("ROLE_ADMIN".equals(role)) {
+                Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                newRoles.add(adminRole);
+            } else {
+                Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                newRoles.add(userRole);
+            }
+        });
+        return newRoles;
     }
 }
